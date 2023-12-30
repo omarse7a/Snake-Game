972208f441115ea86309be1ca4f81be3976cd73c
Omar Sameh <omar.seha90@gmail.com>
2023-12-30 20:07:41 +0200

    Added score-dependent difficulty modifier

--- GameManager.cpp
+++ GameManager.cpp
@@ -14,7 +14,15 @@ void GameManager::run()
 	
 	while (!snakePtr->game_is_over()) {
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 }); //instead of clear screen
-		
+		//Adjusting difficulty
+		if (playerPtr->getScore() < 100)
+			Sleep(150);
+		else if (playerPtr->getScore() < 250)
+			Sleep(100);
+		else if (playerPtr->getScore() < 500)
+			Sleep(60);
+		else
+			Sleep(30);
 		mapPtr->setMap();
 		snakePtr->setOnMap(mapPtr);
 		snakePtr->getDirection();
